a9479d93df96a942cd35d3742efcdda557b4a417
JoaoVDS21 <joao.programmerS2@gmail.com>
2023-01-04 23:03:18 -0300

    First commit app

---
+++ src/Main/index.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { Container, CategoriesContainer, MenuContainer, Footer, FooterContainer } from "./styles";
+
+import { Header } from "../components/Header";
+import { Categories } from "../components/Categories";
+import { Menu } from "../components/Menu";
+import { Button } from "../components/Button";
+import { TableModal } from "../components/TableModal";
+
+export function Main(){
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('')
+
+  function handleSabeTable(table: string) {
+    setSelectedTable(table)
+  }
+
+  return (
+    <>
+      <Container>
+        <Header />
+
+        <CategoriesContainer>
+          <Categories></Categories>
+        </CategoriesContainer>
+
+        <MenuContainer>
+          <Menu></Menu>
+        </MenuContainer>
+
+      </Container>
+
+      <Footer>
+        <FooterContainer>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
+        </FooterContainer>
+      </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSabeTable}
+      />
+    </>
+  );
+}
